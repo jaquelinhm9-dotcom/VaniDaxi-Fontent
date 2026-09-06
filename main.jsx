9ca1c1a59228d4e257601586c6b8c160967b4f50
jaquelinhm9-dotcom <jaquelinhm9@gmail.com>
2026-09-06 07:54:17 -0700

    Update routing base URL and handle redirect path

--- main.jsx
+++ main.jsx
@@ -1,23 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-
 import App from "./App.jsx";
 import "./styles.css";
+
+const base = import.meta.env.BASE_URL.replace(/\/$/, "");
 
 const redirectPath = new URLSearchParams(window.location.search).get("p");
 
 if (redirectPath) {
+  const cleanPath = redirectPath.startsWith("/")
+    ? redirectPath
+    : `/${redirectPath}`;
+
   window.history.replaceState(
-    null,
+    {},
     "",
-    `/VaniDaxi-Fontent${redirectPath}`
+    `${base}${cleanPath}${window.location.hash}`
   );
 }
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="/VaniDaxi-Fontent">
+    <BrowserRouter basename={base}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
