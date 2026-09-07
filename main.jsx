195fe116203f0e84dabf931730adada468b8f1ec
jaquelinhm9-dotcom <jaquelinhm9@gmail.com>
2026-09-07 07:25:01 -0700

    Wire reference mobile navigation and visual layer

--- main.jsx
+++ main.jsx
@@ -2,7 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
+import ReferenceMobileNav from "./ReferenceMobileNav.jsx";
 import "./styles.css";
+import "./reference-mobile.css";
 
 const base = import.meta.env.BASE_URL.replace(/\/$/, "");
 const redirectPath = new URLSearchParams(window.location.search).get("p");
@@ -23,6 +25,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter basename={base}>
       <App />
+      <ReferenceMobileNav />
     </BrowserRouter>
   </React.StrictMode>
 );
