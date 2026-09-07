6156b6ffaf2a3a0e28c041b8b347aa573b8f4efa
jaquelinhm9-dotcom <jaquelinhm9@gmail.com>
2026-09-07 16:58:06 -0700

    feat: initialize VaniDaxi API persistence before app render

--- main.jsx
+++ main.jsx
@@ -2,5 +2,12 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './styles.css'
+import { hydrateVaniDaxi, startVaniDaxiSync } from './api.js'
 
-createRoot(document.getElementById('root')).render(<React.StrictMode><App /></React.StrictMode>)
+async function bootstrap() {
+  await hydrateVaniDaxi()
+  startVaniDaxiSync()
+  createRoot(document.getElementById('root')).render(<React.StrictMode><App /></React.StrictMode>)
+}
+
+bootstrap()
