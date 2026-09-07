aa6c710ba9384cfedba29149c0f6b8b7c4413ca8
jaquelinhm9-dotcom <jaquelinhm9@gmail.com>
2026-09-07 08:02:22 -0700

    Use clean VaniDaxi application entrypoint

--- main.jsx
+++ main.jsx
@@ -2,31 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
-import ReferenceMobileNav from "./ReferenceMobileNav.jsx";
 import "./styles.css";
-import "./reference-mobile.css";
-import "./reference-mobile-final.css";
 
 const base = import.meta.env.BASE_URL.replace(/\/$/, "");
 const redirectPath = new URLSearchParams(window.location.search).get("p");
-
 if (redirectPath) {
-  const cleanPath = redirectPath.startsWith("/")
-    ? redirectPath
-    : `/${redirectPath}`;
-
-  window.history.replaceState(
-    {},
-    "",
-    `${base}${cleanPath}${window.location.hash}`
-  );
+  const cleanPath = redirectPath.startsWith("/") ? redirectPath : `/${redirectPath}`;
+  window.history.replaceState({}, "", `${base}${cleanPath}${window.location.hash}`);
 }
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter basename={base}>
       <App />
-      <ReferenceMobileNav />
     </BrowserRouter>
   </React.StrictMode>
 );
