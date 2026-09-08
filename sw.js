82530aa392e39f89afa6311a9c990325bfb73ce3
jaquelinhm9-dotcom <jaquelinhm9@gmail.com>
2026-09-08 01:22:00 -0700

    Bust stale VaniDaxi service worker cache

--- sw.js
+++ sw.js
@@ -1,9 +1,20 @@
-const CACHE = 'vanidaxi-v1'
-self.addEventListener('install', event => { event.waitUntil(caches.open(CACHE).then(cache => cache.add('./'))) })
-self.addEventListener('activate', event => { event.waitUntil(self.clients.claim()) })
+const CACHE = 'vanidaxi-v2'
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.add('./')).then(() => self.skipWaiting()))
+})
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))
+      .then(() => self.clients.claim())
+  )
+})
 self.addEventListener('fetch', event => {
   if (event.request.method !== 'GET') return
-  event.respondWith(fetch(event.request).then(response => {
-    const copy = response.clone(); caches.open(CACHE).then(cache => cache.put(event.request, copy)); return response
-  }).catch(() => caches.match(event.request).then(cached => cached || caches.match('./'))))
+  event.respondWith(
+    fetch(event.request).then(response => {
+      const copy = response.clone()
+      caches.open(CACHE).then(cache => cache.put(event.request, copy))
+      return response
+    }).catch(() => caches.match(event.request).then(cached => cached || caches.match('./')))
+  )
 })
