b21e52068e6d0d8e07d0733ffa7f4f6f10b6d573
jaquelinhm9-dotcom <jaquelinhm9@gmail.com>
2026-09-08 15:53:23 -0700

    fix stale service worker cache

--- sw.js
+++ sw.js
@@ -1,20 +1,34 @@
-const CACHE = 'vanidaxi-v2'
+const CACHE = 'vanidaxi-v3'
+
 self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE).then(cache => cache.add('./')).then(() => self.skipWaiting()))
+  event.waitUntil(
+    caches.open(CACHE)
+      .then(() => self.skipWaiting())
+  )
 })
+
 self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))
       .then(() => self.clients.claim())
   )
 })
+
 self.addEventListener('fetch', event => {
   if (event.request.method !== 'GET') return
+  const url = new URL(event.request.url)
+  if (url.pathname.endsWith('/') || url.pathname.endsWith('/index.html') || url.pathname.endsWith('/sw.js')) {
+    event.respondWith(fetch(event.request, { cache: 'no-store' }).catch(() => caches.match(event.request)))
+    return
+  }
   event.respondWith(
     fetch(event.request).then(response => {
-      const copy = response.clone()
-      caches.open(CACHE).then(cache => cache.put(event.request, copy))
+      if (response.ok) {
+        const copy = response.clone()
+        caches.open(CACHE).then(cache => cache.put(event.request, copy)).catch(() => {})
+      }
       return response
-    }).catch(() => caches.match(event.request).then(cached => cached || caches.match('./')))
+    }).catch(() => caches.match(event.request))
   )
 })
