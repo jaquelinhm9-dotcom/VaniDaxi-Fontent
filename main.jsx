aac72dd99e660315b4295f260d8cec47ee9cb758
jaquelinhm9-dotcom <jaquelinhm9@gmail.com>
2026-09-08 16:49:20 -0700

    Use the new VaniDaxi marketplace as the main application

--- main.jsx
+++ main.jsx
@@ -1,9 +1,7 @@
 import React from'react'
 import{createRoot}from'react-dom/client'
 import App from'./App.jsx'
-import SellerPortal from'./SellerPortal.jsx'
-import AdminPortal from'./AdminPortal.jsx'
 import'./vanidaxi-new.css'
 const root=document.getElementById('root')
 if(!root)throw new Error('No se encontró #root')
-createRoot(root).render(<React.StrictMode><AdminPortal><SellerPortal><App/></SellerPortal></AdminPortal></React.StrictMode>)
+createRoot(root).render(<React.StrictMode><App/></React.StrictMode>)
