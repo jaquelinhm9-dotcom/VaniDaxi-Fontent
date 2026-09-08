41aaaf22f4461f2fc3529d79501927c9067142be
jaquelinhm9-dotcom <jaquelinhm9@gmail.com>
2026-09-07 18:00:48 -0700

    Wire seller portal into app shell

--- main.jsx
+++ main.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles.css'
 import './welcome-enhancer.css'
+import './seller.css'
 import { hydrateVaniDaxi, startVaniDaxiSync } from './api.js'
 import { fetchCatalog, startCommercialSync } from './commercialApi.js'
 import { observeWelcome } from './welcome-enhancer.js'
+import SellerPortal from './SellerPortal.jsx'
 
 async function bootstrap() {
   await hydrateVaniDaxi()
@@ -14,7 +16,7 @@ async function bootstrap() {
   const module = await import('./AppFixed.jsx')
   const App = module.default
   startVaniDaxiSync()
-  createRoot(document.getElementById('root')).render(<React.StrictMode><App /></React.StrictMode>)
+  createRoot(document.getElementById('root')).render(<React.StrictMode><SellerPortal><App /></SellerPortal></React.StrictMode>)
   observeWelcome()
   startCommercialSync(() => window.__VANI_COMMERCIAL_CATALOG__ || [])
 }
