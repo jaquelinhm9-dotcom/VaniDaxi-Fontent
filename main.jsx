a75b426a844019a7f35c8de8a39111c8443f15cb
jaquelinhm9-dotcom <jaquelinhm9@gmail.com>
2026-09-07 17:47:52 -0700

    Render app after loading commercial catalog

--- main.jsx
+++ main.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './AppFixed.jsx'
 import './styles.css'
 import './welcome-enhancer.css'
 import { hydrateVaniDaxi, startVaniDaxiSync } from './api.js'
@@ -11,6 +10,9 @@ async function bootstrap() {
   await hydrateVaniDaxi()
   const catalog = await fetchCatalog().catch(() => ({ categories: [], products: [] }))
   window.__VANI_COMMERCIAL_CATALOG__ = catalog.products || []
+  window.__VANI_COMMERCIAL_CATEGORIES__ = catalog.categories || []
+  const module = await import('./AppFixed.jsx')
+  const App = module.default
   startVaniDaxiSync()
   createRoot(document.getElementById('root')).render(<React.StrictMode><App /></React.StrictMode>)
   observeWelcome()
