0065093ed36e397a7b53265091c4bfb0301e3119
jaquelinhm9-dotcom <jaquelinhm9@gmail.com>
2026-09-08 22:11:00 -0700

    Enable Supabase Passkey support

--- supabaseClient.js
+++ supabaseClient.js
@@ -10,5 +10,6 @@ export const supabase = createClient(supabaseUrl, supabasePublishableKey, {
     persistSession: true,
     autoRefreshToken: true,
     detectSessionInUrl: true,
+    experimental: { passkey: true },
   },
 })
