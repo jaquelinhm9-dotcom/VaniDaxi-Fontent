8472e6c97706988b926a17d7aa07354d7b060a5d
jaquelinhm9-dotcom <jaquelinhm9@gmail.com>
2026-09-08 18:03:02 -0700

    Mount functional account experience

--- main.jsx
+++ main.jsx
@@ -2,6 +2,7 @@ import React from'react'
 import{createRoot}from'react-dom/client'
 import VaniDaxiShell from'./VaniDaxiShell.jsx'
 import'./vanidaxi-new.css'
+import'./accountEnhancer.js'
 const root=document.getElementById('root')
 if(!root)throw new Error('No se encontró #root')
 createRoot(root).render(<React.StrictMode><VaniDaxiShell/></React.StrictMode>)
