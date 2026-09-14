408d3d547d07439555ef425bb0d96b24611f52f8
jaquelinhm9-dotcom <jaquelinhm9@gmail.com>
2026-09-14 06:53:07 -0700

    Remove legacy welcome realism stylesheet import

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './styles.css'
-import './welcome-realism.css'
 import './welcome-overhaul.css'
 
 createRoot(document.getElementById('root')).render(
