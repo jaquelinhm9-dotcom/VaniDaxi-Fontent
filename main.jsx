d6e2b84cc63ed877e73dd8f5f0c6fd2bc2fa0e01
jaquelinhm9-dotcom <jaquelinhm9@gmail.com>
2026-09-14 00:27:13 -0700

    Integrar retorno verificado de pagos

--- main.jsx
+++ main.jsx
@@ -1,6 +1,7 @@
 import React from'react'
 import{createRoot}from'react-dom/client'
 import VaniDaxiShell from'./VaniDaxiShell.jsx'
+import PaymentReturn from'./PaymentReturn.jsx'
 import'./vanidaxi-new.css'
 import'./responsive-fixes.css'
 import'./accountEnhancer.js'
@@ -9,4 +10,5 @@ import'./seller-customer-enhancer.js'
 import'./admin-returns-enhancer.js'
 const root=document.getElementById('root')
 if(!root)throw new Error('No se encontró #root')
-createRoot(root).render(<React.StrictMode><VaniDaxiShell/></React.StrictMode>)
+const hasPaymentReturn=()=>new URLSearchParams(location.search).has('payment')
+createRoot(root).render(<React.StrictMode>{hasPaymentReturn()?<PaymentReturn/>:<VaniDaxiShell/>}</React.StrictMode>)
