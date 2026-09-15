4e25b0cefcc324a42dcb547bb98659101e8f9804
jaquelinhm9-dotcom <jaquelinhm9@gmail.com>
2026-09-15 00:44:48 -0700

    Clean welcome imports and remove legacy layout patch

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './styles.css'
 import './welcome.css'
-import './welcome-layout-fix.css'
 import './auth-redesign.css'
 
 createRoot(document.getElementById('root')).render(
